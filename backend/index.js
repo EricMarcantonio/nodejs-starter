63466e984b510c28d267ed0629eadfa1b54cfb7d
Eric Marcantonio <eric11marcantonio@gmail.com>
2022-10-12 20:00:02 -0400

    2

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,5 @@
 // This is the start of your backend server. We will write server side code here
+
+const express = require("express")
+
+const app = express()
---
+++ steps.bash
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Step 1
+cd backend
+npm init -y
+
+# Step 2
+cd backend
+npm install express
